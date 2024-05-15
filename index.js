fee556afad92790b8128b436fb1d69438579304f
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-05-15 19:43:47 +0600

    modify purches collection data

--- index.js
+++ index.js
@@ -237,18 +237,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/purchase/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log('Delete ID:', id);
-            const result = await PurchaseCollection.deleteOne({ _id: new ObjectId(id) });
-            res.send(result);
-        });
-
-
-
-
-
-
+       
 
 
 
